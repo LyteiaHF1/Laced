072b0d5a1b3424e289bdae5cc06cd24944962dfe
Lyteia Kitchen <lytehighfashion@gmail.com>
2015-03-31 12:52:57 -0400

    Laced Repo
    Login with Facebook & twitter connected to SQLAlchemy db for now! Page
    working already connected with paypal button for now. All routes are
    working and closet page has basic look and feel (background color,
    profile pic, trade upload button works half way takes to page but image
    upload still has to be done)

---
+++ Laced/app.py
@@ -0,0 +1,120 @@
+from flask import Flask, redirect, url_for, render_template, session
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, UserMixin, login_user, logout_user,\
+    current_user
+from oauth import OAuthSignIn
+import mysql.connector
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['OAUTH_CREDENTIALS'] = {
+    'facebook': {
+        'id': '903491159707098',
+        'secret': 'd5cabb938679cc675b6363c5d0dbff8e'
+    },
+    'twitter': {
+        'id': 'S7mOaScN2xYfhsulJ3bD45xj4',
+        'secret': 'M7nm9itITXPfOIadGcHgYZviTuCElqeTIH3vCmIKKFyP6Ej8ui'
+    }
+}
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.login_view = 'index'
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated():
+        return render_template('closet.html')
+    else:
+        return render_template('index.html')
+
+    
+@app.route('/home')
+def home():
+    #dispaly  trades added to trade floor and shop items
+    db = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='8889', database='Laced')
+    cur = db.cursor()
+    #selects product, price and image form database
+    cur.execute('select productName, price from store')
+    data = cur.fetchall()
+    return render_template('home.html', pagedata = data)
+    
+@app.route('/store')
+def shop():
+#display fruits
+    db = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='8889', database='Laced')
+    cur = db.cursor()
+    #selects product, price and image form database
+    cur.execute('select productName, price, img from store')
+    data = cur.fetchall()
+    return render_template('shop.html', pagedata = data)
+
+@app.route('/trade')
+def trade():
+    #display trades
+    db = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='8889', database='Laced')
+    cur = db.cursor()
+    #selects product, price and image form database
+    cur.execute('select tradeName, size, con from trade')
+    data = cur.fetchall()
+    return render_template('trade.html', pagedata = data)
+
+@app.route('/closet')
+def closet():
+    return render_template('closet.html')
+
+@app.route('/tradeupload')
+def tradeupload():
+        return render_template('tradeuplaodform.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous():
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous():
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
